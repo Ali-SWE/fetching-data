752d3713b828d26aa72831af743e20104d3ea861
Ali Alsaihati <ali.y.alsaihati@gmail.com>
2024-02-18 23:20:48 +0300

    Done !!!

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ const fetchData = async() => {
   /// Looping over rows and pages
   
   let isEnable = true;
-  while(isEnable){
+  while(isEnable){    
+  
     const trElements = await tbodyElement.$$('tr'); // These are all rows
     for (const tr of trElements) {
       const tdElement = await tr.$('td[data-property="meetingTime"]'); // Select the td element with data-property="meetingTime"
@@ -109,13 +110,7 @@ function addSections(sectionCollection, data){
   const startTime = formatTime(data.substring(data.indexOf("SMTWTFS") + 7, data.indexOf(" -")))
   const endTime = formatTime(data.substring(data.indexOf("-") + 2 , data.indexOf(" Type")))
 
-  // sectionCollection.push({
-  //   days: days,
-  //   buildingNumber: building,
-  //   roomNumber: room,
-  //   startTime: startTime,
-  //   endTime: endTime
-  // })
+
   for (const day of days) {
     let dayFound = false
     for (const d in sectionCollection) {
@@ -131,7 +126,11 @@ function addSections(sectionCollection, data){
             for(const r in roomObject){
               if(room == r){
                 roomFound = true
-                roomObject[room].push([startTime,endTime])
+                const timeIntervalList = roomObject[room]
+                const newInterval = [startTime,endTime]
+                if(!existInterval(timeIntervalList, newInterval)){
+                  timeIntervalList.push(newInterval)
+                }
               }
             }
             if(!roomFound){
@@ -176,18 +175,27 @@ function addBuildingsAndRooms(buildingCollection, data) {
 
 function formatTime(time){
   let hours = parseInt(time.substr(0,2))
-  let minutes = parseInt(time.substr(time.indexOf(":") + 1, 2))
+  let minutes = time.substr(time.indexOf(":") + 1, 2)
 
   if (hours === 12) {
     hours = (time.includes("AM")) ? 0 : 12;
   } else {
     hours += (time.includes("PM")) ? 12 : 0;
   }  
+  hours = hours.toString()
+  return hours + ":" + minutes
+}
 
-  const date = new Date()
-  date.setHours(hours, minutes, 0)
-
-  return date
+function existInterval(timeIntervalList, newInterval){
+  const newStartTime = newInterval[0]
+  const newEndTime = newInterval[1]
+  for (const interval of timeIntervalList) {
+    const oldStartTime = interval[0]
+    const oldEndTime = interval[1]
+    if( newStartTime === oldStartTime && newEndTime === oldEndTime)
+    return true
+  }
+  return false
 }
 
 function saveToJsonFile(filename, data){
@@ -219,92 +227,3 @@ function saveToJsonFile(filename, data){
     console.error(error);
   }
 })();
-
-// const obj = [{U: []}, {M: []}, {T: []}, {W: []}, {R: []}]
-// const obj = {U: 
-//             {
-//               "22":
-//                 {"125": [["x","y"], ["c","v"]],
-//                 "130": ["a","s"]    
-//                 }  
-//               ,
-//               "59":
-//                 {"125": [["x","y"], ["c","v"]],
-//                 "130": ["a","s"]    
-//                 }
-//               }
-//             ,
-//             M:1,
-//             T: 99,
-//             W:
-//               {
-//               "22":
-//                 {"125": [["x","y"], ["c","v"]],
-//                 "130": ["a","s"]    
-//                 } 
-//               },
-//             R: 12
-//             }
-
-// const collection = {"U": []}
-
-
-// const days = ["U","W"]
-// const building =  22
-// const room = "125"
-// const startTime = "ali"
-// const endTime = "mustafa"
-// for (const day of days) {
-//   let dayFound = false
-//   for (const key in obj) {
-//     if(key === day){
-//       dayFound = true
-//       const buildingObject = obj[key]
-//       let buildingFound = false
-//       for(const b in buildingObject){
-//         if(building == b){
-//           buildingFound = true
-//           const roomObject = buildingObject[b]
-//           let roomFound = false
-//           for(const r in roomObject){
-//             if(room === r){
-//               roomFound = true
-//               roomObject[room].push([startTime,endTime])
-//             }
-//           }
-//           if(!roomFound){
-//             roomObject[room] = [[startTime,endTime]]
-//           }
-//         }
-//       }
-//       if(!buildingFound){
-//         buildingObject[building] = {room: [[startTime, endTime]]}
-//       }
-//     }
-//   }
-//   if(!dayFound){
-//     obj[day] = {
-//                 day: 
-//                     { 
-//                       building: 
-//                               {
-//                                 room: [[startTime, endTime]]
-//                               }
-//                     }
-//                 }
-//   }
-// }
-
-// const d1 = new Date()
-// d1.setHours(17,30,0)
-// const d2 = new Date()
-// d2.setHours(11,30,0)
-
-// console.log(d1.getHours()===d2.getHours() && d1.getMinutes() === d2.getMinutes())
-// console.log(d2)
-
-
-
-
-
-
